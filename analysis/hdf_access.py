85726b6350a52f9116c66e072aba4d5bfbcbce73
Glen Kirkup <glen.kirkup@flightdataservices.com>
2011-11-08 08:30:36 -0600

    Updated hdf_file object with get_param_data and set_param_data.

--- analysis/hdf_access.py
+++ analysis/hdf_access.py
@@ -1,7 +1,7 @@
+import os
 import h5py #TODO: or pytables for masked array support?
 import numpy as np
 import simplejson as json
-
 
 class hdf_file(object):    # rare case of lower case?!
     """ usage example:
@@ -14,46 +14,55 @@ class hdf_file(object):    # rare case of lower case?!
     hdf['series']['Altitude AAL']['mask'] (Bool list)
     hdf['series']['Altitude AAL'].attrs['limits'] (json)
     """
+    # HDF file settings should be consistent, therefore hardcoding defaults.
+    DATASET_KWARGS = {'compression': 'gzip', 'compression_opts': 3}
+    
     def __repr__(self):
-        # TODO: Glen to put a nice pretty representation of this object
-        return ''
+        '''
+        TODO: Glen to put a nice pretty representation of this object
+        Q: What else should be displayed?
+        '''
+        filename = os.path.dirname(self.hdf.filename)
+        return '%s (%d parameters)' % (filename, len(self))
     
     def __init__(self, file_path):
         self.file_path = file_path
-        self.hdf = None
+        self.hdf = h5py.File(self.file_path, 'r+')
                 
     def __enter__(self):
-        self.open()
-        return self.hdf
+        '''
+        HDF file is opened by __init__.
+        '''
+        pass
 
     def __exit__(self, a_type, value, traceback):
         self.close()
         
     def __getitem__(self, key):
-        """ Allows for: hdf['Altitude AAL'][:30]
-        """
+        '''
+        Allows for dictionary access: hdf['Altitude AAL'][:30]
+        '''
         return self.get_param_data(key)
         
     def __setitem__(self, key, value):
         """ Allows for: hdf['Altitude AAL'] = np.ma.array()
         """
         return self.set_param_data(key, value)
-        
-    def open(self):
-        self.hdf = h5py.File(self.file_path, 'r+')
     
     def close(self):
-        self.hdf.flush() # required?
+        self.hdf.flush() # Q: required?
         self.hdf.close()
         
     def get_param_list(self):
-        """ List of parameters stored in hdf file
-        """
+        '''
+        List of parameters stored in hdf file
+        '''
         return self.hdf['series'].keys()
     
     def get_params(self, params=None):
-        """ Returns params that are available, `ignores` those that aren't
-        """
+        '''
+        Returns params that are available, `ignores` those that aren't
+        '''
         if not params:
             params = self.get_param_list()
         d = {}
@@ -66,29 +75,72 @@ class hdf_file(object):    # rare case of lower case?!
 
     def get_param_data(self, name):
         """ name e.g. "Head Mag"
-        Returns masked array. If mask has been stored, returns it - otherwise returns False
-        
-        Uses np.ma.getmaskarray() to ensure the mask is fully fledged to the
-        length of the data (opposite of shrink_mask())
+        Returns a masked_array. If 'mask' is stored it will be the mask of the
+        returned masked_array, otherwise it will be False.
         """
-        ##return np.ma.array(self.hdf['series'][name]['data'], 
-                           ##mask=self.hdf['series'][name].get('mask', False))
-
-        data = self.hdf['series'][name]['data']
-        mask = np.ma.getmaskarray(self.hdf['series'][name].get('mask', data))
+        param = self.hdf['series'][name]
+        data = param['data']
+        mask = param.get('mask', False)
+        # Using np.ma.getmaskarray() to ensure the mask is fully fledged to the
+        # length of the data (opposite of shrink_mask()) should be unnecessary.
+        #mask = np.ma.getmaskarray(self.hdf['series'][name].get('mask', data).value)
         return np.ma.array(data, mask=mask)
+    
+    def keys(self):
+        '''
+        Parameter group names within the series group.
+        
+        :returns: List of parameter names.
+        :rtype: list of str
+        '''
+        return self.hdf['series'].keys()
+    
+    def __len__(self):
+        '''
+        Number of parameter groups within the series group.
+        
+        :returns: Number of parameters.
+        :rtype: int
+        '''
+        return len(self.hdf['series'])
 
     def set_param_data(self, name, array):
         """
-        reshape data as required and store.
-        """
-        self.hdf['series'][name]['data'] = array.data
-        if hasattr(array, 'mask'):
-            # store the shrunk array to save space    #TODO: How much does this really save?
-            self.hdf['series'][name]['mask'] = array.shrink_mask().mask
-            
-        #TODO: Possible to store validity percentage upon name.attrs
+        reshape data as required and store.        
         
+        :param name: Name of parameter. Forward slashes are not allowed within an HDF identifier as it supports filesystem-style indexing, e.g. '/series/CAS'.
+        :type name: str
+        :param array: Array containing data and potentially a mask for the data.
+        :type array: np.array or np.ma.masked_array
+        """
+        # Allow both arrays and masked_arrays.
+        if hasattr(array, 'mask'):
+            data = array.data
+            # store the shrunk mask to save space #Q: How much does this really save?
+            mask = array.shrink_mask().mask
+        else:
+            data = array
+            mask = None
+        # Either get or create parameter.
+        series = self.hdf['series']
+        if name in series:
+            param = series[name]
+            # Dataset must be deleted before recreation.
+            del param['data']
+        else:
+            param = series.create_group(name)
+        dataset = param.create_dataset('data', data=array, 
+                                       **self.DATASET_KWARGS)
+        if 'mask' in param:
+            # Existing mask will no longer reflect the new data.
+            del param['mask']
+        # Q: Should we create a mask of False by default?
+        if mask is not None: # Testing is None as shrunk mask may simply be False.
+            # Dataset compression options can only be provided for an
+            # array-like object and not a single value.
+            kwargs = {} if isinstance(mask, np.bool_) else self.DATASET_KWARGS
+            mask_dataset = param.create_dataset('mask', data=mask, **kwargs)
+        #TODO: Possible to store validity percentage upon name.attrs
     
     def set_param_limits(self, name, limits):
         """
@@ -110,12 +162,32 @@ class hdf_file(object):    # rare case of lower case?!
             return None
 
 
-def print_hdf_info(hdf):
-    # Glen to fill this gap!
-    #pprint some interesting stuff
-    pass
+def print_hdf_info(hdf_file):
+    hdf_file = hdf_file.hdf
+    series = hdf_file['series']
+    # IOLA
+    # 8.0
+    if 'Time' in series:
+        print 'Tailmark:', hdf_file.attrs['tailmark']
+        print 'Start Time:', hdf_file.attrs['starttime']
+        print 'End Time:', hdf_file.attrs['endtime']
+    
+    for group_name, group in series.iteritems():
+        print '[%s]' % group_name
+        print 'Frequency:', group.attrs['frequency']
+        print group.attrs.items()
+        # IOLA's latency is our frame offset.
+        print 'Offset:', group.attrs['latency']
+        print 'External Data Type:', group.attrs['external_datatype']
+        print 'External Data Format:', group.attrs['external_dataformat']
+        print 'Number of recorded values:', len(group['data'])
+    #param_series = hdf_file['series'][parameter]
+    #data = param_series['data']
 
 if __name__ == '__main__':
+    import sys
+    print_hdf_info(hdf_file(sys.argv[1]))
+    sys.exit()
     file_path = 'AnalysisEngine/resources/data/hdf5/flight_1626325.hdf5'    
     with hdf_file(file_path) as hdf:
         print_hdf_info(hdf)
--- tests/hdf_access_test.py
+++ tests/hdf_access_test.py
@@ -2,11 +2,38 @@ try:
     import unittest2 as unittest  # py2.6
 except ImportError:
     import unittest
+import os
+import random
+import errno
+import h5py
 import numpy as np 
 from analysis.hdf_access import hdf_file
 
 
+TEST_DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                             'test_data')
+
 class TestHdfFile(unittest.TestCase):
+    def setUp(self):
+        self.hdf_path = os.path.join(TEST_DATA_DIR, 'test_hdf_access.hdf')
+        hdf = h5py.File(self.hdf_path, 'w')
+        series = hdf.create_group('series')
+        self.param_name = 'TEST_PARAM10'
+        param = series.create_group(self.param_name)
+        self.param_data = np.arange(100)
+        dataset = param.create_dataset('data', data=self.param_data)
+        self.masked_param_name = 'TEST_PARAM11'
+        masked_param = series.create_group(self.masked_param_name)
+        self.param_mask = [bool(random.randint(0, 1)) for x in range(len(self.param_data))]
+        dataset = masked_param.create_dataset('data', data=self.param_data)
+        mask_dataset = masked_param.create_dataset('mask', data=self.param_mask)
+        hdf.close()
+        self.hdf_file = hdf_file(self.hdf_path)
+    
+    def tearDown(self):
+        self.hdf_file.close()
+        os.remove(self.hdf_path)
+    
     def test_open_and_close_and_full_masks(self):
         with hdf_file('blah') as hdf:
             self.assertFalse(hdf.id is None)
@@ -31,4 +58,73 @@ class TestHdfFile(unittest.TestCase):
     
     def test_get_attr(self):
         pass
+    
+    def test_set_param_data(self):
+        self.__test_set_param_data(self.hdf_file.set_param_data)
+    
+    def test___set_item__(self):
+        self.__test_set_param_data(self.hdf_file.__setitem__)
+        
+    def __test_set_param_data(self, set_param_data):
+        '''
+        :param set_param_data: Allows passing of either hdf_file.set_param_data or __getitem__.
+        :type set_param_data: method
+        '''
+        hdf_file = self.hdf_file
+        # Create new parameter with np.array.
+        name1 = 'TEST_PARAM1'
+        array = np.arange(100)
+        set_param_data(name1, array)
+        self.assertTrue(np.all(hdf_file.hdf['series'][name1]['data'].value == array))
+        # Create new parameter with np.ma.masked_array.
+        name2 = 'TEST_PARAM2'
+        mask = False
+        masked_array = np.ma.masked_array(data=array, mask=mask)
+        set_param_data(name2, masked_array)
+        self.assertTrue(np.all(hdf_file.hdf['series'][name2]['data'].value == array))
+        self.assertTrue(hdf_file.hdf['series'][name2]['mask'].value == mask)
+        # Set existing parameter's data with np.array.
+        array = np.arange(200)
+        set_param_data(name1, array)
+        self.assertTrue(np.all(hdf_file.hdf['series'][name1]['data'].value == array))
+        self.assertFalse('mask' in hdf_file.hdf['series'][name1])
+        # Set existing parameter's data with np.ma.masked_array.
+        mask = [bool(random.randint(0, 1)) for x in range(len(array))]
+        masked_array = np.ma.masked_array(data=array, mask=mask)
+        set_param_data(name1, masked_array)
+        self.assertTrue(np.all(hdf_file.hdf['series'][name1]['data'].value == array))
+        self.assertTrue(np.all(hdf_file.hdf['series'][name1]['mask'].value == mask))
+    
+    def test_get_param_data(self):
+        self.__test_get_param_data(self.hdf_file.get_param_data)
+    
+    def test___get_item__(self):
+        self.__test_get_param_data(self.hdf_file.__getitem__)
+    
+    def __test_get_param_data(self, get_param_data):
+        '''
+        :param set_param_data: Allows passing of either hdf_file.get_param_data or __getitem__.
+        :type get_param_data: method
+        '''
+        # Create new parameter with np.array.
+        data = get_param_data(self.param_name)
+        self.assertTrue(np.all(self.param_data == data))
+        # Create new parameter with np.array.
+        data = get_param_data(self.masked_param_name)
+        self.assertTrue(np.all(self.param_data == data.data))
+        self.assertTrue(np.all(self.param_mask == data.mask))
+    
+    def test_len(self):
+        '''
+        Depends upon HDF creation in self.setUp().
+        '''
+        self.assertEqual(len(self.hdf_file), 2)
+    
+    def test_keys(self):
+        '''
+        Depends upon HDF creation in self.setUp().
+        '''
+        self.assertEqual(sorted(self.hdf_file.keys()),
+                         sorted([self.param_name, self.masked_param_name]))
+    
     
