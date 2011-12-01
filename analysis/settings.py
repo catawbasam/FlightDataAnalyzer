45edcb51724e9cf74fa7d258cb2850bd4b62723e
Chris Jesse <chris.jesse@flightdataservices.com>
2011-12-01 15:09:21 +0000

    renamed MODULES to NODE_MODULES in settings.py

--- analysis/settings.py
+++ analysis/settings.py
@@ -2,11 +2,11 @@
 
 # Modules to import all derived Nodes from. Additional modules can be
 # appended to this list in local_settings.py
-MODULES = ['analysis.derived_parameters',
-           'analysis.key_point_values', 
-           'analysis.key_time_instances',
-           'analysis.sections',
-           'analysis.flight_phase']
+NODE_MODULES = ['analysis.derived_parameters',
+                'analysis.key_point_values', 
+                'analysis.key_time_instances',
+                'analysis.sections',
+                'analysis.flight_phase']
 
 ##########################
 ## Parameter Analysis
