5143e8b95bfb416d6ecdde74dde0520b923508ef
Martin Wimpress <martin.wimpress@flightdataservices.com>
2011-12-15 11:31:40 +0000

    Added pip requirements

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
-import thingy
+
 setup(name='analysis',
-      version=analysis.__version__,
-      py_modules=['analysis'],
-      )
+    version=analysis.__version__,
+    py_modules=['analysis'],
+)
