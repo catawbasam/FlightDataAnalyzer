bc5e4de58c2855ba432d8cbd85d80cd83cc48430
Chris Jesse <chris.jesse@flightdataservices.com>
2012-02-17 19:39:01 +0000

    allow for none in array before align

--- analysis_engine/split_segments.py
+++ analysis_engine/split_segments.py
@@ -416,7 +416,11 @@ def append_segment_info(hdf_segment_path, segment_type, segment_slice, part):
         onehz = P(frequency = 1)
         dt_arrays = []
         for name in ('Year', 'Month', 'Day', 'Hour', 'Minute', 'Second'):
-            dt_arrays.append(align(hdf.get(name), onehz, signaltype='Multi-State'))
+            p = hdf.get(name)
+            if p:
+                dt_arrays.append(align(p, onehz, signaltype='Multi-State'))
+            else:
+                dt_arrays.append(None)
         
         # establish timebase for start of data
         try:
