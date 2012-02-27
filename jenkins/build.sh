628720484219f4225048f8ddb91887c12dd99923
Martin Wimpress <martin.wimpress@flightdataservices.com>
2012-02-27 15:50:26 +0000

    Update jenkins scripts

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,12 +1,56 @@
 #!/usr/bin/env bash
 
+function usage() {
+    local MODE=${1}
+    echo "Usage"
+    echo
+    echo "  ${0} -v python_version"
+    echo
+    echo "Required parameters"
+    echo "  -v python version : The virtualenv Python version to use."
+    echo "Optional parameters"
+    echo "  -h                : This help"
+    exit 1
+}
+
+# Make sure we are running from within Jenkins
+if [ -z "${WORKSPACE}" ]; then
+    echo "ERROR! This script is designed to run from within a Jenkins build."
+    exit1
+fi
+
+# Init the variables
+VIRTVER=""
+
+# Parse the options
+OPTSTRING=hv:
+while getopts ${OPTSTRING} OPT
+do
+    case ${OPT} in
+        h) usage;;
+        v) VIRTVER=${OPTARG};;
+        *) usage;;
+    esac
+done
+shift "$(( $OPTIND - 1 ))"
+
+VIRTENV=${WORKSPACE}/.py${VIRTVER}
+
+# Check the virtualenv exists
+if [ ! -f ${VIRTENV}/bin/python${VIRTVER} ]; then
+    echo "ERROR! Couldn't find the virtualenv : ${VIRTENV}"
+    exit 1
+fi
+
 # Enter the virtualenv
-VIRTENV=${WORKSPACE}/.pyenv
 . ${VIRTENV}/bin/activate
+
+# Enter the Jenkins workspace
 cd ${WORKSPACE}
 
 # Remove previous builds
-rm -rfv ${WORKSPACE}/dist/* || :
+rm -rf ${WORKSPACE}/dist/*
+rm -rf ${WORKSPACE}/doc/build/*
 
 # Make a source distribution
 if [ -f setup.py ] && [ -f setup.cfg ]; then
@@ -39,7 +83,6 @@ if [ -f setup.py ] && [ -f setup.cfg ]; then
 
     # Build sphinx documentation
     if [ -f ${WORKSPACE}/doc/Makefile ]; then
-        cd ${WORKSPACE}
         python setup.py build_sphinx
     fi
 fi
