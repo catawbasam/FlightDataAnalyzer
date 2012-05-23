b6bd6971ed08de70e32236dc8334900f5f7f1642
Marek Petlicki <marek.petlicki@flightdataservices.com>
2012-05-23 12:00:14 +0100

    Fixes in derived parameters: Flaps and AltitudeRadio

--- analysis_engine/derived_parameters.py
+++ analysis_engine/derived_parameters.py
@@ -514,19 +514,19 @@ class AltitudeRadio(DerivedParameterNode):
                 merge_two_parameters(source_A, source_B)
         
         elif frame_name in ['737-5']:
-            if 'Altitude_Radio_D226A101_1_16D' in frame_qualifier:
+            if frame_qualifier and 'Altitude_Radio_D226A101_1_16D' in frame_qualifier:
                 # Compute nonlinear rad alt from recorded value
                 source_A.array = alt_rad_non_linear(source_A.array, 'D226A101_1_16D')
                 source_B.array = alt_rad_non_linear(source_B.array, 'D226A101_1_16D')
                 self.array, self.frequency, self.offset = \
                     blend_two_parameters(source_A, source_B)
 
-            elif 'Altitude_Radio_EFIS' in frame_qualifier:
+            elif frame_qualifier and 'Altitude_Radio_EFIS' in frame_qualifier:
                 self.array, self.frequency, self.offset = \
                     blend_two_parameters(source_C, source_D)
                 self.array = np.ma.masked_greater(self.array, 2600)
 
-            elif 'Altitude_Radio_None' in frame_qualifier:
+            elif frame_qualifier and 'Altitude_Radio_None' in frame_qualifier:
                 pass # Some old 737 aircraft have no rad alt recorded.
 
             else:
@@ -1523,7 +1523,7 @@ class FlapLever(DerivedParameterNode):
     def derive(self, flap=P('Flap Lever Position'), series=A('Series'), family=A('Family')):
         try:
             flap_steps = get_flap_map(series.value, family.value) 
-        except ValueError:
+        except KeyError:
             # no flaps mapping, round to nearest 5 degrees
             logging.warning("No flap settings - rounding to nearest 5")
             # round to nearest 5 degrees
@@ -1579,21 +1579,15 @@ class Flap(DerivedParameterNode):
     """
     Steps raw Flap angle from surface into detents.
     """
-    @classmethod
-    def can_operate(cls, available):
-        # works with any combination of params available
-        return True
-
     def derive(self, flap=P('Flap Surface'),
-               series=A('Series'), family=A('Family'),
-               flap_steps=A('Flap Selections')):
+               series=A('Series'), family=A('Family')):
         
         """
         Steps raw Flap angle into detents.
         """
         try:
             flap_steps = get_flap_map(series.value, family.value) 
-        except:
+        except KeyError:
             # no flaps mapping, round to nearest 5 degrees
             logging.warning("No flap settings - rounding to nearest 5")
             # round to nearest 5 degrees
@@ -1609,7 +1603,7 @@ class Slat(DerivedParameterNode):
     def derive(self, slat=P('Slat Surface'), series=A('Series'), family=A('Family')):
         try:
             slat_steps = get_slat_map(series.value, family.value) 
-        except ValueError:
+        except KeyError:
             # no slats mapping, round to nearest 5 degrees
             logging.warning("No slat settings - rounding to nearest 5")
             # round to nearest 5 degrees
