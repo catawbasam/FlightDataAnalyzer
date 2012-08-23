50910efe6f29e27d8b10b12152ebfde471b6397b
Rob Parker <rob.parker@flightdataservices.com>
2012-08-23 10:51:47 +0100

    added series mapping coverage script

---
+++ utils/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) Flight Data Services Ltd
+# http://www.flightdataservices.com
+# See the file "LICENSE" for the full license governing this code.
+
+
+
+################################################################################
+# vim:et:ft=python:nowrap:sts=4:sw=4:ts=4
