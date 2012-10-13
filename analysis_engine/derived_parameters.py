6f8747f0d5a9649b331dac7f81fa3be35249e04f
Dave Jesse <dave.jesse@flightdataservices.com>
2012-10-13 14:29:17 +0100

    Turbulence algorithm amended to align with ATR specification, and test case added.
    Default width added to rate_of_change_array to simplify correlation definitions.

--- analysis_engine/derived_parameters.py
+++ analysis_engine/derived_parameters.py
@@ -3497,10 +3497,14 @@ class TurbulenceRMSG(DerivedParameterNode):
     """
     name = 'Turbulence RMS g'
     def derive(self, acc=P('Acceleration Vertical')):
-        acc_sq = (acc.array - 1.0)**2.0
-        mean_sq = moving_average(acc_sq, window=int(acc.frequency*5+1))
-        self.array = np.ma.sqrt(mean_sq)
-
+        width=int(acc.frequency*5+1)
+        mean = moving_average(acc.array, window=width)
+        acc_sq = (acc.array)**2.0
+        n__sum_sq = moving_average(acc_sq, window=width)
+        # Rescaling required as moving average is over width samples, whereas
+        # we have only width - 1 gaps; fences and fence posts again !
+        core = (n__sum_sq - mean**2.0)*width/(width-1.0)
+        self.array = np.ma.sqrt(core)
 
 #------------------------------------------------------------------
 # WIND RELATED PARAMETERS
