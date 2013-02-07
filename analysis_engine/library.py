index fix for _value which should not include the decimal part of slice.start as the numpy slicing floors the starting index. This also fixes the max_abs_value which returned an incorrect value after using index_at_value which interpolates between two values.

analysis_engine/library.py
@@ -1488,7 +1488,7 @@ def runway_deviation(array, runway={}, heading=0.0):
     
     NOTE: Uses heading supplied in preference to coordinates.
     
-    :param array: array of TRUE heading values
+    :param array: array or Value of TRUE heading values
     :type array: Numpy masked array (usually already sliced to relate to the landing in question).
     :param runway: runway details.
     :type runway: dict (runway.value if this is taken from an attribute).
@@ -2752,7 +2752,7 @@ def max_abs_value(array, _slice=slice(None), start_edge=None, stop_edge=None):
         if edge_value and edge_value > value:
             index = stop_edge
             value = edge_value
-    return Value(index, value_at_index(array, index)) # Recover sign of the value.
+    return Value(index, array[index]) # Recover sign of the value.
 
 
 def max_value(array, _slice=slice(None), start_edge=None, stop_edge=None):
@@ -4318,7 +4318,8 @@ def _value(array, _slice, operator):
     if _slice.step and _slice.step < 0:
         raise ValueError("Negative step not supported")
     if np.ma.count(array[_slice]):
-        index = operator(array[_slice]) + (_slice.start or 0) * (_slice.step or 1)
+        # floor the start position as it will have been floored during the slice
+        index = operator(array[_slice]) + floor(_slice.start or 0) * (_slice.step or 1)
         value = array[index]
         return Value(index, value)
     else:

tests/library_test.py
@@ -2165,8 +2165,21 @@ class TestMaxAbsValue(unittest.TestCase):
         self.assertEqual(max_abs_value(array), (100, -40))
         array = array*-1.0
         self.assertEqual(max_abs_value(array), (100, 40))
-
-
+        
+    def test_max_abs_slice_decimal(self):
+        "where slice start is not a whole number, ensure maxima is returned"
+        d = np.ma.array(data = [
+            -0.000, -0.000, -0.352, -0.000, -0.000,  0.351,  0.351, 
+            -0.000, -0.000, -0.352, -0.352, -0.352, -0.000, -0.000,
+            -0.352, -0.352, -0.352, -0.001, -0.001, -0.001, -0.001,
+             0.351,  0.351,  0.351, -0.001, -0.001, -0.352, -0.352,
+            -0.001, -0.352, -0.704, -1.056, -1.055, -0.704, -0.704])
+        s = slice(3.482, 1241.611)
+        res = max_abs_value(d, s)
+        self.assertEqual(res.value, -1.056)
+        self.assertEqual(res.index, 31)
+        
+        
 class TestMergeSources(unittest.TestCase):
     def test_merge_sources_basic(self):
         p1 = np.ma.array([0]*4)