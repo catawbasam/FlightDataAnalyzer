catawbasam/FlightDataAnalyzer

commit 744a977a294e9e3b62df90e090314b2f7d1e0fde

Changes for 767 frames
Some frame specific changes added, and Latitude/Longitude Coarse for
frame with very coarse, low sample rate position data added.

analysis_engine/derived_parameters.py
@@ -46,6 +46,7 @@ from analysis_engine.library import (actuator_mismatch,
                                      machtat2sat,
                                      mask_inside_slices,
                                      mask_outside_slices,
+                                     merge_sources,
                                      merge_two_parameters,
                                      moving_average,
                                      np_ma_ones_like,
@@ -852,6 +853,7 @@ class AltitudeRadio(DerivedParameterNode):
                source_A = P('Altitude Radio (A)'),
                source_B = P('Altitude Radio (B)'),
                source_C = P('Altitude Radio (C)'),
+               source_E = P('Altitude Radio EFIS'),
                source_L = P('Altitude Radio EFIS (L)'),
                source_R = P('Altitude Radio EFIS (R)')):
 
@@ -900,6 +902,17 @@ class AltitudeRadio(DerivedParameterNode):
         elif frame_name in ['CRJ-700-900', 'E135-145']:
             self.array, self.frequency, self.offset = \
                 blend_two_parameters(source_A, source_B)
+        
+        elif frame_name in ['2227000-59B']:
+            # The four sources, Left, Centre, EFIS and Right are sampled in every frame.
+            self.array = repair_mask(merge_sources(source_A.array, 
+                                                   source_B.array, 
+                                                   source_E.array, 
+                                                   source_C.array)
+                                     )
+            self.frequency = source_A.frequency * 4.0
+            self.offset = source_A.offset
+            
         else:
             raise DataFrameError(self.name, frame_name)
 
@@ -1423,22 +1436,15 @@ class BrakePressure(DerivedParameterNode):
     This node allows for expansion for different types, and possibly
     operation in primary and standby modes.
     """
-    align_to_first_dependency = False
+    #align_to_first_dependency = False
 
-    @classmethod
-    def can_operate(cls, available):
-        return ('Brake (L) Press' in available and \
-                'Brake (R) Press' in available)
+    #@classmethod
+    #def can_operate(cls, available):
+        #return ('Brake (L) Press' in available and \
+                #'Brake (R) Press' in available)
 
-    def derive(self, brake_L=P('Brake (L) Press'), brake_R=P('Brake (R) Press'),
-               frame=A('Frame')):
-        frame_name = frame.value if frame else ''
-
-        if frame_name.startswith('737-'):
-            self.array, self.frequency, self.offset = blend_two_parameters(brake_L, brake_R)
-
-        else:
-            raise DataFrameError(self.name, frame_name)
+    def derive(self, brake_L=P('Brake (L) Press'), brake_R=P('Brake (R) Press')):
+        self.array, self.frequency, self.offset = blend_two_parameters(brake_L, brake_R)
 
 ################################################################################
 # Pack Valves
@@ -2855,7 +2861,7 @@ class FlapSurface(DerivedParameterNode):
                frame=A('Frame'), apps=S('Approach'), alt_aal=P('Altitude AAL')):
         frame_name = frame.value if frame else ''
 
-        if frame_name.startswith('737-') or frame_name in ['757-DHL']:
+        if frame_name.startswith('737-') or frame_name in ['757-DHL', '767-232F_Delta-85']:
             self.array, self.frequency, self.offset = blend_two_parameters(flap_A,
                                                                            flap_B)
 
@@ -3722,6 +3728,9 @@ class MagneticVariation(DerivedParameterNode):
 class VerticalSpeedInertial(DerivedParameterNode):
     '''
     See 'Vertical Speed' for pressure altitude based derived parameter.
+    
+    If the aircraft records an inertial vertical speed, rename this "Vertical
+    Speed Inertial - Recorded" to avoid conflict
 
     This routine derives the vertical speed from the vertical acceleration, the
     Pressure altitude and the Radio altitude.
@@ -4474,6 +4483,10 @@ class Aileron(DerivedParameterNode):
             self.array = al.array if al else ar.array
             self.frequency = al.frequency if al else ar.frequency
             self.offset = al.offset if al else ar.offset
+        elif ali and alo and ari and aro:
+            self.array = merge_sources(ali.array, ari.array, alo.array,aro.array)
+            self.frequency = ali.frequency * 4.0
+            self.offset = ali.offset
         else:
             return NotImplemented
 
@@ -4647,6 +4660,26 @@ class SpeedbrakeSelected(MultistateDerivedParameterNode):
             raise ValueError("Can't work without either Speedbrake or Handle")
         return array
 
+    def b767_speedbrake(self, handle):
+        '''
+        Speedbrake Handle Positions for 767:
+
+            ========    ============
+              %           Notes
+            ========    ============
+               0.0        Full Forward
+              15.0        Armed
+             100.0        Full Up
+            ========    ============
+        '''
+        # Speedbrake Handle only
+        armed = np.ma.where((12.0 < handle.array) & (handle.array < 25.0),
+                            'Armed/Cmd Dn', 'Stowed')
+        array = np.ma.where(handle.array >= 25.0,
+                            'Deployed/Cmd Up', armed)
+        return array
+
+
     def derive(self,
             deployed=M('Speedbrake Deployed'),
             armed=M('Speedbrake Armed'),
@@ -4665,9 +4698,11 @@ class SpeedbrakeSelected(MultistateDerivedParameterNode):
             array[deployed.array == 'Deployed'] = 2
             self.array = array # (only call __set_attr__ once)
 
-        elif frame_name.startswith('737-'):
+        elif spdbrk and handle:
             self.array = self.b737_speedbrake(spdbrk, handle)
 
+        elif handle:
+            self.array = self.b767_speedbrake(handle)
         else:
             # Not implemented for this frame
             raise DataFrameError(self.name, frame_name)
@@ -4695,38 +4730,30 @@ class StickShaker(MultistateDerivedParameterNode):
     def can_operate(cls, available):
         '''
         '''
-        # NOTE: Does not take into account which parameter for which frame!
-        return 'Frame' in available and (
-            'Stick Shaker (L)' in available or \
-            'Stick Shaker (R)' in available or \
-            'Shaker Activation' in available \
-        )
+        return ('Stick Shaker (L)' in available or \
+                'Shaker Activation' in available
+                )
 
-    def derive(self, frame=A('Frame'),
-            shake_l=M('Stick Shaker (L)'),
+    def derive(self, shake_l=M('Stick Shaker (L)'),
             shake_r=M('Stick Shaker (R)'),
             shake_act=M('Shaker Activation')):
         '''
         '''
-        frame_name = frame.value if frame else ''
-
-        if frame_name in ['CRJ-700-900'] and shake_act:
-            self.array, self.frequency, self.offset = \
-                shake_act.array, shake_act.frequency, shake_act.offset
-
-        elif frame_name in ['737-1', '737-3', '737-3A', '737-3B', '737-3C', '737-4',
-                            '737-7','737-i', '737-2227000-335A', '757-DHL']:
+        if shake_l and shake_r:
             self.array = np.ma.logical_or(shake_l.array, shake_r.array)
             self.frequency , self.offset = shake_l.frequency, shake_l.offset
-
-        elif frame_name in ['737-5', '737-5_NON-EIS'] and shake_l:
+        
+        elif shake_l:
             # Named (L) but in fact (L) and (R) are or'd together at the DAU.
             self.array, self.frequency, self.offset = \
                 shake_l.array, shake_l.frequency, shake_l.offset
+        
+        elif shake_act:
+            self.array, self.frequency, self.offset = \
+                shake_act.array, shake_act.frequency, shake_act.offset
 
-        # Stick shaker not found in 737-6 frame.
         else:
-            raise DataFrameError(self.name, frame_name)
+            raise NotImplementedError
 
 
 class ApproachRange(DerivedParameterNode):

analysis_engine/key_point_values.py
@@ -3217,22 +3217,32 @@ class LatitudeAtLiftoff(KeyPointValueNode):
     def can_operate(cls, available):
         '''
         '''
-        one_of = lambda *names: any(name in available for name in names)
-        return 'Liftoff' in available and any((
-            'Latitude' in available,
-            one_of('AFR Takeoff Runway', 'AFR Takeoff Airport'),
-        ))
+        return 'Liftoff' in available and any_of(('Latitude',
+                                                  'Latitude Coarse',
+                                                  'AFR Takeoff Runway', 
+                                                  'AFR Takeoff Airport'),
+                                                 available)
 
     def derive(self,
             lat=P('Latitude'),
             liftoffs=KTI('Liftoff'),
             toff_afr_apt=A('AFR Takeoff Airport'),
-            toff_afr_rwy=A('AFR Takeoff Runway')):
+            toff_afr_rwy=A('AFR Takeoff Runway'),
+            lat_c=P('Latitude Coarse')):
         '''
+        Note that Latitude Coarse is a superframe parameter with poor
+        resolution recorded on some FDAUs. Keeping it at the end of the list
+        of parameters means that it will be aligned to a higher sample rate
+        rather than dragging other parameters down to its sample rate. See
+        767 Delta data frame.
         '''
         # 1. Attempt to use latitude parameter if available:
         if lat:
             self.create_kpvs_at_ktis(lat.array, liftoffs)
+            return
+        
+        if lat_c:
+            self.create_kpvs_at_ktis(lat_c.array, liftoffs)
             return
 
         value = None
@@ -3276,24 +3286,30 @@ class LongitudeAtLiftoff(KeyPointValueNode):
     def can_operate(cls, available):
         '''
         '''
-        one_of = lambda *names: any(name in available for name in names)
-        return 'Liftoff' in available and any((
-            'Longitude' in available,
-            one_of('AFR Takeoff Runway', 'AFR Takeoff Airport'),
-        ))
-
+        return 'Liftoff' in available and any_of(('Longitude',
+                                                  'Longitude Coarse',
+                                                  'AFR Takeoff Runway',
+                                                  'AFR Takeoff Airport'),
+                                                 available)
+    
     def derive(self,
             lon=P('Longitude'),
             liftoffs=KTI('Liftoff'),
             toff_afr_apt=A('AFR Takeoff Airport'),
-            toff_afr_rwy=A('AFR Takeoff Runway')):
+            toff_afr_rwy=A('AFR Takeoff Runway'),
+            lon_c=P('Longitude Coarse')):
         '''
+        See note relating to coarse latitude and longitude under Latitude At Takeoff
         '''
         # 1. Attempt to use longitude parameter if available:
         if lon:
             self.create_kpvs_at_ktis(lon.array, liftoffs)
             return
 
+        if lon_c:
+            self.create_kpvs_at_ktis(lon_c.array, liftoffs)
+            return
+            
         value = None
 
         # 2a. Attempt to use longitude of runway midpoint:
@@ -3767,7 +3783,7 @@ class EngGasTempDuringEngStartMax(KeyPointValueNode):
             return None, None
         # Ideally we'd use a shorter timebase, e.g. 2 seconds, but N2 is only
         # sampled at 1/4Hz on some aircraft:
-        n2_rate = rate_of_change(n2, 4)
+        n2_rate = rate_of_change(n2, 8)
         # The engine only accelerates through 30% when starting. Let's find the
         # last time it did this before taking off:
         passing_30 = index_at_value(n2.array, 30.0, slice(idx, 0, -1))

analysis_engine/library.py
@@ -1965,6 +1965,12 @@ def gtp_compute_error(weights, *args):
     frequency = args[6]
     mode = args[7]
     return_arg_set = args[8]
+    
+    if len(speed)==0:
+        if return_arg_set == 'iterate':
+            return 0.0
+        else:
+            return lat, lon, 0.0
 
     speed_weighting  = gtp_weighting_vector(speed, straight_ends, weights)
     if mode == 'takeoff':

commit 88f38572021789b66ea4a8201e75bf3a468d81d0

parameters: fix speedbrake broken in 744a977a.
We need to find a better way of handling per-frame/family/series code in
these nodes that require different routines.

analysis_engine/derived_parameters.py
@@ -4681,31 +4681,32 @@ class SpeedbrakeSelected(MultistateDerivedParameterNode):
 
 
     def derive(self,
-            deployed=M('Speedbrake Deployed'),
-            armed=M('Speedbrake Armed'),
-            handle=P('Speedbrake Handle'),
-            spdbrk=P('Speedbrake'),
-            frame=A('Frame')):
-        frame_name = frame.value if frame else ''
+               deployed=M('Speedbrake Deployed'),
+               armed=M('Speedbrake Armed'),
+               handle=P('Speedbrake Handle'),
+               spdbrk=P('Speedbrake'),
+               family=A('Family')):
+
+        family_name = family.value if family else ''
+
         if deployed:
-            # Speedbrake Deployed available, use this
-            # set initial state to 'Stowed'
+            # We have a speedbrake deployed discrete. Set initial state to
+            # stowed, then set armed states if available, and finally set
+            # deployed state:
             array = np.ma.zeros(len(deployed.array))
             if armed:
-                # If we have Armed, set this first
                 array[armed.array == 'Armed'] = 1
-            # deployed will overide some armed states
             array[deployed.array == 'Deployed'] = 2
-            self.array = array # (only call __set_attr__ once)
+            self.array = array
 
-        elif spdbrk and handle:
+        elif family_name == 'B737':
             self.array = self.b737_speedbrake(spdbrk, handle)
 
-        elif handle:
+        elif family_name == 'B767':
             self.array = self.b767_speedbrake(handle)
+
         else:
-            # Not implemented for this frame
-            raise DataFrameError(self.name, frame_name)
+            raise NotImplementedError
 
 
 ################################################################################