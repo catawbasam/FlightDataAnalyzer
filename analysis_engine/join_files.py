578f056af6cac68f1fe3d86fb6cb84c732d13cc0
Chris Jesse <chris.jesse@flightdataservices.com>
2013-03-19 09:47:20 +0000

    join_files now supports copying of file upon creation

--- analysis_engine/join_files.py
+++ analysis_engine/join_files.py
@@ -1,8 +1,21 @@
 from hdfaccess.utils import concat_hdf
+from flightdatautilities.filesystem_tools import copy_file
 
-def join_files(first_part, second_part):
+def join_files(first_part, second_part, copy=True):
     """
-    Flight Joining
+    Flight Joining of two parts. Expected for joining a START_ONLY and
+    STOP_ONLY segment together.
+    
+    Q: Support deleting of first and second part?
+    
+    :param first_part: Path to first hdf file.
+    :type first_part: String (path)
+    :param second_part: Path to first hdf file.
+    :type second_part: String (path)
     """
-    hdf_path = concat_hdf([first_part, second_part], dest=first_part) 
+    if copy:
+        dest = copy_file(first_part, postfix='_joined')
+    else:
+        dest = first_part
+    hdf_path = concat_hdf([first_part, second_part], dest=dest)
     return hdf_path
