6fc4a45561a676cd0fc37d6a4ffd9488ae23edcf
Chris Jesse <chris.jesse@flightdataservices.com>
2013-03-20 23:36:19 +0000

    Approach and Landing cleanup

--- analysis_engine/flight_phase.py
+++ analysis_engine/flight_phase.py
@@ -197,15 +197,11 @@ class ApproachAndLanding(FlightPhaseNode):
 
     def derive(self, alt_aal=P('Altitude AAL For Flight Phases'),
                lands=S('Landing'), go_arounds=S('Go Around And Climbout')):
-        # Prepare to extract the slices
-        app_slices = FlightPhaseNode()
-        ga_slices = FlightPhaseNode()
-
         for land in lands:
             app_start = index_closest_value(
                 alt_aal.array, INITIAL_APPROACH_THRESHOLD,
                 slice(land.lower_bound, 0, -1))
-            app_slices.add(Section(app_start, land.upper_bound))
+            self.add(Section(app_start, land.upper_bound))
 
         last_ga = 0
         for ga in go_arounds:
@@ -218,16 +214,13 @@ class ApproachAndLanding(FlightPhaseNode):
             gapp_start = index_closest_value(
                 alt_aal.array, INITIAL_APPROACH_THRESHOLD,
                 slice(ga.lower_bound, last_ga, -1))
-            ga_slices.add(Section(gapp_start, ga.upper_bound))
+            self.add(Section(gapp_start, ga.upper_bound))
             last_ga = ga.upper_bound
 
-        all_apps = app_slices | ga_slices
-
-        if not all_apps:
+        if not self:
             self.warning('Flight with no valid approach or go-around phase. '
                          'Probably truncated data')
-        self.update(all_apps)
-
+            
 
 class Approach(FlightPhaseNode):
     """
