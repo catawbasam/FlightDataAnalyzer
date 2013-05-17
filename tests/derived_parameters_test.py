188cb1b5d4de510b5fc82df5ce416f6e062d506e
Nick Pope <nick.pope@flightdataservices.com>
2013-05-17 15:13:11 +0100

    parameters: add support for propeller speed.

--- tests/derived_parameters_test.py
+++ tests/derived_parameters_test.py
@@ -71,6 +71,9 @@ from analysis_engine.derived_parameters import (
     Eng_N3Avg,
     Eng_N3Max,
     Eng_N3Min,
+    Eng_NpAvg,
+    Eng_NpMax,
+    Eng_NpMin,
     Eng_VibN1Max,
     Eng_VibN2Max,
     Eng_VibN3Max,
@@ -1747,8 +1750,82 @@ class TestEng_N3Min(unittest.TestCase):
             np.array([999, # both masked, so filled with 999
                       1,2,3,4,5,6,7,8,9])
         )
-        
-        
+
+
+class TestEng_NpAvg(unittest.TestCase):
+    def test_can_operate(self):
+        opts = Eng_NpAvg.get_operational_combinations()
+        self.assertEqual(opts[0], ('Eng (1) Np',))
+        self.assertEqual(opts[-1], ('Eng (1) Np', 'Eng (2) Np', 'Eng (3) Np', 'Eng (4) Np'))
+        self.assertEqual(len(opts), 15) # 15 combinations accepted!
+
+
+    def test_derive_two_engines(self):
+        # this tests that average is performed on incomplete dependencies and
+        # more than one dependency provided.
+        a = np.ma.array(range(0, 10))
+        b = np.ma.array(range(10,20))
+        a[0] = np.ma.masked
+        b[0] = np.ma.masked
+        b[-1] = np.ma.masked
+        eng_avg = Eng_NpAvg()
+        eng_avg.derive(P('a',a), P('b',b), None, None)
+        ma_test.assert_array_equal(
+            np.ma.filled(eng_avg.array, fill_value=999),
+            np.array([999, # both masked, so filled with 999
+                      6,7,8,9,10,11,12,13, # unmasked avg of two engines
+                      9]) # only second engine value masked
+        )
+
+
+class TestEng_NpMax(unittest.TestCase):
+    def test_can_operate(self):
+        opts = Eng_NpMax.get_operational_combinations()
+        self.assertEqual(opts[0], ('Eng (1) Np',))
+        self.assertEqual(opts[-1], ('Eng (1) Np', 'Eng (2) Np', 'Eng (3) Np', 'Eng (4) Np'))
+        self.assertEqual(len(opts), 15) # 15 combinations accepted!
+
+    def test_derive_two_engines(self):
+        # this tests that average is performed on incomplete dependencies and
+        # more than one dependency provided.
+        a = np.ma.array(range(0, 10))
+        b = np.ma.array(range(10,20))
+        a[0] = np.ma.masked
+        b[0] = np.ma.masked
+        b[-1] = np.ma.masked
+        eng = Eng_NpMax()
+        eng.derive(P('a',a), P('b',b), None, None)
+        ma_test.assert_array_equal(
+            np.ma.filled(eng.array, fill_value=999),
+            np.array([999, # both masked, so filled with 999
+                      11,12,13,14,15,16,17,18,9])
+        )
+
+
+class TestEng_NpMin(unittest.TestCase):
+    def test_can_operate(self):
+        opts = Eng_NpMin.get_operational_combinations()
+        self.assertEqual(opts[0], ('Eng (1) Np',))
+        self.assertEqual(opts[-1], ('Eng (1) Np', 'Eng (2) Np', 'Eng (3) Np', 'Eng (4) Np'))
+        self.assertEqual(len(opts), 15) # 15 combinations accepted!
+
+    def test_derive_two_engines(self):
+        # this tests that average is performed on incomplete dependencies and
+        # more than one dependency provided.
+        a = np.ma.array(range(0, 10))
+        b = np.ma.array(range(10,20))
+        a[0] = np.ma.masked
+        b[0] = np.ma.masked
+        b[-1] = np.ma.masked
+        eng = Eng_NpMin()
+        eng.derive(P('a',a), P('b',b), None, None)
+        ma_test.assert_array_equal(
+            np.ma.filled(eng.array, fill_value=999),
+            np.array([999, # both masked, so filled with 999
+                      1,2,3,4,5,6,7,8,9])
+        )
+
+
 class TestFlap(unittest.TestCase):
     def test_can_operate(self):
         opts = Flap.get_operational_combinations()
