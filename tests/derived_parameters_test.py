31b51e902572a46e44e0d4c54266cb378285b7fb
Chris Jesse <chris.jesse@flightdataservices.com>
2013-09-12 12:07:22 +0100

    removed flaperon from derived test

--- tests/derived_parameters_test.py
+++ tests/derived_parameters_test.py
@@ -18,8 +18,7 @@ from analysis_engine.library import (align,
                                      max_value,
                                      np_ma_masked_zeros,
                                      np_ma_masked_zeros_like,
-                                     np_ma_ones_like,
-                                     unique_values)
+                                     np_ma_ones_like)
 
 from analysis_engine.node import (Attribute,
                                   A,
@@ -2850,28 +2849,7 @@ class TestAileronTrim(unittest.TestCase):
     @unittest.skip('Test Not Implemented')
     def test_derive(self):
         self.assertTrue(False, msg='Test not implemented.')
-        
 
-class TestFlaperon(unittest.TestCase):
-    def test_can_operate(self):
-        self.assertTrue(Flaperon.can_operate(
-            ('Aileron (L)', 'Aileron (R)'),
-            series=Attribute('Series', 'A330-200'),
-            family=Attribute('Family', 'A330')))
-        
-    def test_derive(self):
-        al = load(os.path.join(test_data_path, 'aileron_left.nod'))
-        ar = load(os.path.join(test_data_path, 'aileron_right.nod'))
-        series = A('Series', 'A330-200')
-        family = A('Family', 'A330')
-        flaperon = Flaperon()
-        flaperon.derive(al, ar, series, family)
-        # ensure values are grouped into aileron settings accordingly
-        # flaperon is now step at movement start
-        self.assertEqual(unique_values(flaperon.array.astype(int)),
-                         [(0, 22056), (5, 291), (10, 1205)])
-        
-        
 
 class TestAirspeedMinusV2For3Sec(unittest.TestCase):
     def test_can_operate(self):
